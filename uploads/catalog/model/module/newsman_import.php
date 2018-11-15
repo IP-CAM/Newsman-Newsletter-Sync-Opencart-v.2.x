1e7233aaafb78479f423c8684ae4edafb8c506b3
Corodeanu Lucian <lucianfcorodeanu@gmail.com>
2018-11-15 14:05:43 +0100

    Cron updates

--- uploads/catalog/model/module/newsman_import.php
+++ uploads/catalog/model/module/newsman_import.php
@@ -14,6 +14,7 @@ class ModelModuleNewsmanImport extends Model
 	{
 		$this->load->model('setting/setting');
 		$data = (array)$this->model_setting_setting->getSetting('newsman_import');
+
 		if (isset($data['last_data_time']) && $data['last_data_time'] < date("Y-m-d H:i:s", strtotime('-1 hour')))
 		{
 			if (isset($data['api_key'], $data['user_id'], $data['list_id'], $data['import_type']))
@@ -22,14 +23,14 @@ class ModelModuleNewsmanImport extends Model
 				$client = new Newsman_Client($data['user_id'], $data['api_key']);
 				if ($data['import_type'] == 1)
 				{
-					$query = $this->db->query("SELECT COUNT(email) AS number FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1");
+					$query = $this->db->query("SELECT COUNT(email) AS number FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1 AND newsletter = 1");
 					$rounds = ceil(intval($query->row['number']) / 10000);
 					for ($i = 0; $i < $rounds; $i++)
 					{
-						$query = $this->db->query("SELECT email FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1 ORDER BY customer_id ASC LIMIT " . ($i * 10000) . ", 10000");
-						$csv = "email" . PHP_EOL;
+						$query = $this->db->query("SELECT email FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1 AND newsletter = 1 ORDER BY customer_id ASC LIMIT " . ($i * 10000) . ", 10000");
+						$csv = "email" . "," . "source" . PHP_EOL;
 						foreach ($query->rows as $row)
-							$csv .= $row['email'] . PHP_EOL;
+							$csv .= $row['email'] . "," . "opencart newsman plugin subscriber" . PHP_EOL;
 						$client->import->csv($data['list_id'], '', $csv);
 					}
 					return true;
@@ -44,14 +45,14 @@ class ModelModuleNewsmanImport extends Model
 							{
 								if ($cg != "'0'")
 								{
-									$query = $this->db->query("SELECT COUNT(email) AS number FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1 AND customer_group_id = " . $cg);
+									$query = $this->db->query("SELECT COUNT(email) AS number FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1 AND newsletter = 1 AND customer_group_id = " . $cg);
 									$rounds = ceil(intval($query->row['number']) / 10000);
 									for ($i = 0; $i < $rounds; $i++)
 									{
-										$query = $this->db->query("SELECT email, firstname, lastname FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1 AND customer_group_id = " . $cg . " ORDER BY customer_id ASC LIMIT " . ($i * 10000) . ", 10000");
-										$csv = "email,firstname,lastname" . PHP_EOL;
+										$query = $this->db->query("SELECT email, firstname, lastname FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1 AND newsletter = 1 AND customer_group_id = " . $cg . " ORDER BY customer_id ASC LIMIT " . ($i * 10000) . ", 10000");
+										$csv = "email,firstname,lastname,source" . PHP_EOL;
 										foreach ($query->rows as $row)
-											$csv .= $row['email'] . "," . $row['firstname'] . "," . $row['lastname'] . PHP_EOL;
+											$csv .= $row['email'] . "," . $row['firstname'] . "," . $row['lastname'] . "," . "opencart newsman plugin subscriber" . PHP_EOL;
 										$client->import->csv($data['list_id'], array($seg), $csv);
 									}
 								} else
@@ -61,9 +62,9 @@ class ModelModuleNewsmanImport extends Model
 									for ($i = 0; $i < $rounds; $i++)
 									{
 										$query = $this->db->query("SELECT email, firstname, lastname FROM " . DB_PREFIX . "customer WHERE date_added > '" . $data['last_data_time'] . "' AND status = 1 AND approved = 1 AND newsletter = 1 ORDER BY customer_id ASC LIMIT " . ($i * 10000) . ", 10000");
-										$csv = "email,firstname,lastname" . PHP_EOL;
+										$csv = "email,firstname,lastname,source" . PHP_EOL;
 										foreach ($query->rows as $row)
-											$csv .= $row['email'] . "," . $row['firstname'] . "," . $row['lastname'] . PHP_EOL;
+											$csv .= $row['email'] . "," . $row['firstname'] . "," . $row['lastname'] . "," . "opencart newsman plugin subscriber" . PHP_EOL;
 										$client->import->csv($data['list_id'], array($seg), $csv);
 									}
 								}
